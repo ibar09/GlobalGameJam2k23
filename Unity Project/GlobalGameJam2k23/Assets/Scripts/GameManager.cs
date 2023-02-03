610ff6ddee31f775b84ccad5ea89cb675cf088b3
ibar09 <iheb30abdelkader@gmail.com>
2023-02-03 23:43:46 +0100

    new stuff

--- Unity Project/GlobalGameJam2k23/Assets/Scripts/GameManager.cs
+++ Unity Project/GlobalGameJam2k23/Assets/Scripts/GameManager.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
     public Player currentPlayer;
+    public GameObject focusedCamera, mapCmaera;
     public int playersNumber;
     public List<Player> players;
+    public CinemachineVirtualCamera cam;
+
+
+
 
     public void generateMap(int N)
+
     {
         //generate l map hasb aadad l players w yhot kol player f blastou
         playersNumber = N;
@@ -21,18 +27,19 @@ public class GameManager : MonoBehaviour
             currentPlayer = players[players.IndexOf(currentPlayer) + 1];
         else
             currentPlayer = players[0];
+        cam.Follow = currentPlayer.transform;
     }
 
-    /*
-    public bool isMyTurn(Player player)
-    {
-        return currentPlayer == player;
-    }*/
 
     public void Move(int steps)
     {
         currentPlayer.Move(steps);
         NextTurn();
+    }
+    public void SwitchCamera()
+    {
+        focusedCamera.SetActive(!focusedCamera.activeSelf);
+        mapCmaera.SetActive(!mapCmaera.activeSelf);
     }
 
 
--- Unity Project/GlobalGameJam2k23/Assets/Scripts/Player.cs
+++ Unity Project/GlobalGameJam2k23/Assets/Scripts/Player.cs
@@ -4,6 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public enum Directon
+    {
+        UP, DOWN, LEFT, RIGHT
+    }
+    [SerializeField]
+    private float x = 0f;
+    Directon directon;
+    public void DirectionRng()
+    {
+        float downWeight = 0.25f + x;
+        float percentage = Random.Range(0, 1f);
+        Debug.Log(percentage);
+        if (percentage <= downWeight)
+        {
+            x = 0;
+            directon = Directon.DOWN;
+        }
+        else
+        {
+            x += 0.05f;
+            int newRng = Random.Range(1, 4);
+            Debug.Log(newRng);
+            switch (newRng)
+            {
+                case 1:
+                    directon = Directon.UP;
+                    break;
+                case 2:
+                    directon = Directon.LEFT;
+                    break;
+                case 3:
+                    directon = Directon.RIGHT;
+                    break;
+            }
+        }
+    }
+
     public GameManager gameManager;
     public int id;
     private void Start()
@@ -12,6 +49,9 @@ public class Player : MonoBehaviour
     }
     public void Move(int steps)
     {
-        Debug.Log("Player " + id + " has moved " + steps);
+        DirectionRng();
+        Debug.Log("Player " + id + " has moved " + steps + " to " + directon);
     }
+
+
 }
